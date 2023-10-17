dc9d9b64c7887b69cc7645a4a4772df8772d88d6
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-17 18:28:30 +0600

    first commit

--- express-fronted/my-crud-project/src/component/DisplayUser.jsx
+++ express-fronted/my-crud-project/src/component/DisplayUser.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const DisplayUser = () => {
 
   const users=useLoaderData()
+  const [updatedUser,setupdatedUser]=useState(users)
   console.log(users)
   const handelDelete=(_id)=>{
      console.log(_id)
@@ -13,16 +15,22 @@ const DisplayUser = () => {
      .then((res)=>res.json())
      .then((data)=>{
       console.log(data)
+
+     
       if(data.deletedCount>0){
+      
         alert("delete successfully")
+        
       }
+      const filterData=updatedUser.filter(item=>item._id!==_id)
+      setupdatedUser(filterData)
      })
   }
   return (
     <div>
-      <h2>{users._id}</h2>
+      <h2>{updatedUser._id}</h2>
       {
-     users.map((user)=> <div key={user._id}>
+     updatedUser.map((user)=> <div key={user._id}>
       <h1>{user.name}</h1>
       <br />
       <button onClick={()=>handelDelete(user._id)} type="submit">Delete</button>
