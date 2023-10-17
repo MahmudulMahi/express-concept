824d09492494cb862b886814b03b029caeb773db
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-17 13:57:57 +0600

    first commit

---
+++ express-fronted/my-crud-project/src/component/PostUser.jsx
@@ -0,0 +1,38 @@
+
+
+const PostUser = () => {
+  const handlePostUser=e=>{
+    e.preventDefault()
+
+    const form =e.target
+    const name=form.name.value
+    const email=form.email.value
+    const password =form.password.value
+    // console.log(name,email,password)
+
+    const mydata={
+      name,
+      email,
+      password,
+    }
+    console.log(mydata)
+  }
+  return (
+    <div>
+      <h2>user</h2>
+
+      <form onSubmit={handlePostUser} action="">
+
+       <input type="text" name="name" id="" />
+       <br />
+       <input type="email" name="email" id="" />
+       <br />
+       <input type="password" name="password" id="" />
+       <br />
+       <button type="submit"> Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostUser;
