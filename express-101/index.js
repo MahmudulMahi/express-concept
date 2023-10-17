51729e43652e2c7585a253d51f153c7ac4adc715
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-17 17:59:48 +0600

    first commit

--- express-101/index.js
+++ express-101/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors=require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 
 app.use(cors())
@@ -37,6 +37,15 @@ async function run() {
       const user =req.body
       const result =await userCollection.insertOne(user)
       console.log(result)
+      res.send(result)
+    })
+
+    // delete single user
+    app.delete("/users/:id",async(req,res)=>{
+      const id =req.params.id
+      console.log("id",id)
+      const query ={_id:new ObjectId(id)}
+      const result =await userCollection.deleteOne(query)
       res.send(result)
     })
 
--- express-fronted/my-crud-project/src/component/DisplayUser.jsx
+++ express-fronted/my-crud-project/src/component/DisplayUser.jsx
@@ -5,12 +5,27 @@ const DisplayUser = () => {
 
   const users=useLoaderData()
   console.log(users)
+  const handelDelete=(_id)=>{
+     console.log(_id)
+     fetch(`http://localhost:5000/users/${_id}`, {
+      method:"DELETE",
+     })
+     .then((res)=>res.json())
+     .then((data)=>{
+      console.log(data)
+      if(data.deletedCount>0){
+        alert("delete successfully")
+      }
+     })
+  }
   return (
     <div>
       <h2>{users._id}</h2>
       {
      users.map((user)=> <div key={user._id}>
       <h1>{user.name}</h1>
+      <br />
+      <button onClick={()=>handelDelete(user._id)} type="submit">Delete</button>
      </div>)
 
       }
