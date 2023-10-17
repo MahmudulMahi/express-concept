27575c281658ed7ac797f426b97c702149ce92f2
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-17 16:41:14 +0600

    first commit

--- express-fronted/my-crud-project/src/component/DisplayUser.jsx
+++ express-fronted/my-crud-project/src/component/DisplayUser.jsx
@@ -7,7 +7,13 @@ const DisplayUser = () => {
   console.log(users)
   return (
     <div>
-      
+      <h2>{users._id}</h2>
+      {
+     users.map((user)=> <div key={user._id}>
+      <h1>{user.name}</h1>
+     </div>)
+
+      }
     </div>
   );
 };
